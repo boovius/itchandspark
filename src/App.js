b520084863b3c6d6fff95ced6c2881f27e1c25f4
Joshua Book <joshua.book@gmail.com>
2018-11-11 14:22:33 -0500

    add transition between itch and stars images with finger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import itchHand from './assets/images/itchHand.png'
+import finger from './assets/images/finger.png'
+import stars from './assets/images/stars.png'
+import './App.css'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.updateImages = this.updateImages.bind(this)
+    this.state = {
+      itch: true
+    }
+  }
+
+  updateImages() {
+    this.setState({itch: !this.state.itch})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <span>itch</span>
+            <span>&</span>
+            <span>SPARK</span>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div onClick={this.updateImages} className="App-LandingSpot">
+          <ReactCSSTransitionGroup
+            transitionName='fadingImage'
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+          >
+            {this.state.itch &&
+              <img src={itchHand} className="App-ItchHand" alt="hand" />
+            }
+          </ReactCSSTransitionGroup>
+          <img src={finger} className="App-ItchHand" alt="finger-pointing" />
+          <ReactCSSTransitionGroup
+            transitionName='fadingImage'
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+          >
+             {!this.state.itch &&
+               <div className='stars-wrapper'>
+                 <img src={stars} className="App-ItchHand" alt="finger-pointing" />
+               </div>
+             }
+          </ReactCSSTransitionGroup>
+        </div>
       </div>
     );
   }
